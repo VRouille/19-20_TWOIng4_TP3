1485afff5cf0a3c0775f9f6798f83271f6817557
VRouille <vincent.rouille@edu.ece.fr>
2019-10-28 23:57:04 +0100

    2nd commit
    Modification sur API_Weather.js et sur index
    Les 3 jours suivants ne fonctionnent pas encore

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -6,6 +6,8 @@ const API_KEY = "4081444b7b90198136fefe6ed4ccf35b";
 const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
+// URL API THREDAYFORECAST
+const API_URL_THREEDAYFORECAST = "https://api.openweathermap.org/data/2.5/forecast/daily";
 
 
 class API_WEATHER{
@@ -25,8 +27,16 @@ class API_WEATHER{
       crossdomain: true
     })
   }
+
   // Retourne l'element HTML de l'icon symbolisant la méteo.
   getHTMLElementFromIcon(icon){
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
+  }
+
+  fetchThreeDayForecast(){
+    return axios
+    .get(`${API_URL_THREEDAYFORECAST}?q=${this.city}&units=metric&appid=${API_KEY}&cnt=4`, {
+      crossdomain: true
+    })
   }
 }
--- js/script.js
+++ js/script.js
@@ -24,12 +24,35 @@ function start() {
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
       
     })
+
+    apiWeather
+    .getThreeDayForecast()
+    .then(function(response) {
+      // Récupère la donnée d'une API
+      const data = response.data;
+
+      // Boucle for pour les 3 prochains jours
+      for (var id = 1; id <= 4; id++) {
+        
+        // Informations principales
+        const main = data.L[id].weather[0].main;
+        const description = data.L[id].weather[0].description;
+        const temp = data.L[id].main.temp;
+        const icon = apiWeather.getHTMLElementFromIcon(data.L[id].weather[0].icon);
+
+        // Modifie 
+        document.getElementById(`day${id+1}-forecast-main`).innerHTML = main;
+        document.getElementById(`day${id+1}-forecast-more-info`).innerHTML = description;
+        document.getElementById(`day${id+1}-icon-weather-container`).innerHTML = icon;
+        document.getElementById(`day${id+1}-forecast-temp`).innerHTML = `${temp}°C`;
+      }
+
+    })
+
     .catch(function(error) {
       // Affiche une erreur
       console.error(error);
     });
 }
 
-function getThreeDayForecast(){
-  
-}
+
